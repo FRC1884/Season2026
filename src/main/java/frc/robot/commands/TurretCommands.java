606663842658ef892a35baeca4e4aa8711fb4c73
Airmanextra <rish.vasupalli@gmail.com>
2026-01-17 16:43:50 +0000

    SHoot while moving, maintaining accuracy when moving by vector math
    added constant field elements in global constants

--- src/main/java/frc/robot/commands/TurretCommands.java
+++ src/main/java/frc/robot/commands/TurretCommands.java
@@ -2,11 +2,13 @@ package frc.robot.commands;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Config;
 import frc.robot.subsystems.turret.TurretSubsystem;
 import frc.robot.util.TurretUtil;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.DoubleSupplier;
 import java.util.function.Function;
@@ -50,11 +52,30 @@ public final class TurretCommands {
   public static Command shootingWhileMoving(
           TurretSubsystem turret,
           Supplier<Pose2d> robotPoseSupplier,
-          Function<Pose2d, Optional<Translation2d>> targetSupplier) {
+          Function<Pose2d, Optional<Translation2d>> targetSupplier,
+          Supplier<ChassisSpeeds> robotVelocitySupplier,
+          Optional<Alliance> ally,
+          Map<Double, Double> lookupTable )
+{
       return Commands.run(
           () -> {
-              Pose2d robotPose = robotPoseSupplier.get();
+              Alliance alliance = ally.get();
+              Translation2d target = alliance == Alliance.Blue ? new Translation2d(4.63, 4.03) : 
+              new Translation2d(11.9, 4.03);
+              
+              ChassisSpeeds speeds = robotVelocitySupplier.get();
+              Pose2d currentPose = robotPoseSupplier.get();
+              Translation2d currentTranslation = currentPose.getTranslation();
+              double dist = target.getDistance(currentTranslation);
+              double shotTime = ShooterCommands.find(dist);
+              
 
+              double futureX = currentTranslation.getX()+(speeds.vxMetersPerSecond)*shotTime;
+              double futureY = currentTranslation.getY()+(speeds.vyMetersPerSecond)*shotTime;
+              Translation2d futureTranslation = new Translation2d(futureX, futureY);
+              Translation2d newTarget = target.minus(futureTranslation);
           });
+
+          //TODO: Something about a method that returns a command???
   }
 }
