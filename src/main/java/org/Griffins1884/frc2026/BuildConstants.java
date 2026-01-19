0119777df3e9af85c91d6ee64ae4cb5558e3c1e4
jonathanst-georges <JonathanStGeorges@icloud.com>
2026-01-19 15:42:35 +0000

    build.gradle package changes bug

--- src/main/java/org/Griffins1884/frc2026/BuildConstants.java
+++ src/main/java/org/Griffins1884/frc2026/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "season2026";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 225;
-  public static final String GIT_SHA = "635ae49135b29415b2c697abf0fc7e03d5fe92ae";
-  public static final String GIT_DATE = "2026-01-19 06:11:22 EST";
+  public static final int GIT_REVISION = 226;
+  public static final String GIT_SHA = "a3d159cf8e69369a68da577dba7dff9d80c95eca";
+  public static final String GIT_DATE = "2026-01-19 10:39:35 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2026-01-19 07:13:09 EST";
-  public static final long BUILD_UNIX_TIME = 1768824789195L;
+  public static final String BUILD_DATE = "2026-01-19 10:42:11 EST";
+  public static final long BUILD_UNIX_TIME = 1768837331270L;
   public static final int DIRTY = 1;
 
   private BuildConstants() {}
